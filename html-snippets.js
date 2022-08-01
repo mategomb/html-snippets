9ccff78b664e8569e82951537912390288b75460
Mate Gombkoto <mategombhu@gmail.com>
2022-08-01 08:21:22 +0200

    initial commit

---
+++ html-snippets.js
@@ -0,0 +1,16 @@
+// Import and call it in order to find and add snippets after the page has been loaded.
+
+export default async function insertSnippets() {
+	const slotEls = document.querySelectorAll(".js-snippet-slot");
+
+	if (slotEls.length > 0) {
+		for (const slotEl of slotEls) {
+			const snippetName = slotEl.dataset.snippetName;
+			const snippetRes = await fetch(`/snippets/${snippetName}.html`);
+			const snippetText = await snippetRes.text();
+
+			slotEl.insertAdjacentHTML("afterend", snippetText);
+			slotEl.remove();
+		}
+	}
+}

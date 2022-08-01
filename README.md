3b70e09d104be479c04dddb5d48536f320012f99
Mate Gombkoto <mategombhu@gmail.com>
2022-08-01 08:43:50 +0200

    markdown readme and updated commenting

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
+# HTML Snippets
+
 HTML imports are really missing from my vanilla JS workflow, so here's my very simple take on it.
 
 The .html snippet files should be in the "snippets" directory, in the root of your project. Like this:
 /snippets/\*.html
 
 Elements with the class name "js-snippet-slot" get picked up and replaced with their corresponding snippets according to the value of "data-snippet-name":
+
+```
 <any-tag class="js-snippet-slot" data-snippet-name="any-name"></any-tag>
+```
--- demo/html-snippets.js
+++ demo/html-snippets.js
@@ -1,12 +1,10 @@
-// Import and call it in order to find and add snippets after the page has been loaded.
-
 export default async function insertSnippets() {
 	const slotEls = document.querySelectorAll(".js-snippet-slot");
 
 	if (slotEls.length > 0) {
 		for (const slotEl of slotEls) {
 			const snippetName = slotEl.dataset.snippetName;
-			const snippetRes = await fetch(`./snippets/${snippetName}.html`);
+			const snippetRes = await fetch(`./snippets/${snippetName}.html`); // Path is relative here only for demo purposes.
 			const snippetText = await snippetRes.text();
 
 			slotEl.insertAdjacentHTML("afterend", snippetText);
